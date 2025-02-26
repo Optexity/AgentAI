abb9dc5c85066e655247e05a2eb7762d54110018
Sankalp Garg <sankalp2621998@gmail.com>
2025-02-26 12:28:39 -0800

    logging

--- agentai/main.py
+++ agentai/main.py
@@ -1,3 +1,5 @@
+import argparse
+import json
 import os
 
 from agent import BasicAgent
@@ -10,6 +12,7 @@ from computergym import (
     OpenEndedWebsite,
     make_env,
 )
+from computergym.utils import save_screenshot, save_str_obs
 from tqdm import tqdm
 from utils import get_logger
 
@@ -33,7 +36,7 @@ def run(task: AbstractServiceNowTask, log_path="./logs"):
         ],
         cache_dir=log_path,
         goal_message=goal,
-        headless=True,
+        headless=False,
     )
     agent = BasicAgent("basic_agent", env, "basic_agent")
 
@@ -46,6 +49,14 @@ def run(task: AbstractServiceNowTask, log_path="./logs"):
             logger.info("Too many steps, stopping...")
             break
         model_response, action = agent.get_next_action(obs)
+
+        if log_path:
+            cache_dir = os.path.join(log_path, f"step-{env.current_step}")
+            os.makedirs(cache_dir, exist_ok=True)
+            string = model_response.model_dump()
+            string = json.dumps(string, indent=4)
+            save_str_obs(string, cache_dir, f"model-response-{env.current_step}.txt")
+
         logger.info(f"model_response: {model_response}")
         string = action.model_dump()
         string["action_name"] = action.__class__.__name__
@@ -59,27 +70,28 @@ def run(task: AbstractServiceNowTask, log_path="./logs"):
     # release the environment
     ## Validate
     reward, stop, message, info = task.validate(env.page, [])
-    logger.info(f"Reward: {reward}, Stop: {stop}, Message: {message}, Info: {info}")
+    logger.info(f"Final Reward: {reward}")
+    logger.info(f"Final Stop: {stop}")
+    logger.info(f"Final Message: {message}")
+    logger.info(f"Final Info: {info}")
     env.close()
     return reward
 
 
-def main():
-    total_tasks = 0
-    total_reward = 0
-    for seed in tqdm(range(10)):
-        for task_entrypoint in tqdm(SERVICE_CATALOG_TASKS):
-            task = task_entrypoint(seed=seed)
-            log_path = os.path.join(
-                "./logs", task_entrypoint.__name__, f"seed-{str(seed)}"
-            )
-            reward = run(task, log_path)
-            total_reward += reward
-            total_tasks += 1
-    print(
-        f"Total Score: {total_reward}, Total Tasks: {total_tasks}, Average Score: {total_reward / total_tasks}"
+def main(args):
+    task_entrypoint = SERVICE_CATALOG_TASKS[args.task_num]
+    task = task_entrypoint(seed=args.seed)
+    log_path = os.path.join(
+        args.log_path, task_entrypoint.__name__, f"seed-{str(args.seed)}"
     )
+    os.makedirs(log_path, exist_ok=True)
+    reward = run(task, log_path)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Run the browser gym tasks.")
+    parser.add_argument("--log_path", type=str, default="./logs")
+    parser.add_argument("--seed", type=int, required=True)
+    parser.add_argument("--task_num", type=int, required=True)
+    args = parser.parse_args()
+    main(args)
