71a41b778e60bd974b73f4e8cc0d9b824a3550be
Sankalp Garg <sankalp2621998@gmail.com>
2025-03-06 22:52:05 -0800

    inference

--- agentai/slrum_jobs/run_inference.sh
+++ agentai/slrum_jobs/run_inference.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
-#SBATCH --job-name=lamma3_serving
+#SBATCH --job-name=llamma3_serving
 #SBATCH --output=/data/user_data/sachingo/Reinforce-Align-AI/slurm_outputs/%x/%A_%a.out  # Standard output
 #SBATCH --error=/data/user_data/sachingo/Reinforce-Align-AI/slurm_outputs/%x/%A_%a.err   # Standard error
 #SBATCH --partition=general
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:0
-#SBATCH --mem=2G
-#SBATCH --time=1:00:00
-#SBATCH --mail-type=END
+#SBATCH --gres=gpu:A6000:1
+#SBATCH --mem=40G
+#SBATCH --time=00:10:00
 #SBATCH --priority=1
-#SBATCH --array=0-10
+#SBATCH --array=0-1
 
 cd /data/user_data/sachingo/Reinforce-Align-AI
 source ~/.bashrc
 conda activate browsergym
 
-conda env list
-
-python --version
-echo "Running array task ID: $SLURM_ARRAY_TASK_ID"
-echo "Done!"
+cd LLaMA-Factory
+API_PORT=800$SLURM_ARRAY_TASK_ID llamafactory-cli api AgentAI/agentai/inference_configs/llama3.1_lora_sft_service_catalog.yaml
